339cced01b622a2d85805c43084f55684283905d
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-26 14:37:15 -0300

    adjstment package project

--- src/main/java/com/dio/productcatalogtwo/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dio.productcatalogtwo.repository;
-
-import com.dio.productcatalogtwo.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
