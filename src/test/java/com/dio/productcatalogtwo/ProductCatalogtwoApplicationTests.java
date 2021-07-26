d7242293f15378e8756d6608cae3b9b2aca8550c
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-26 10:28:47 -0300

    Create project and ElasticSearch configuration

---
+++ src/test/java/com/dio/productcatalogtwo/ProductCatalogtwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.productcatalogtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogtwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
