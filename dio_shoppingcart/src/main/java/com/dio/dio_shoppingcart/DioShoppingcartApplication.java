c8262c5a810b8a4feff240f8fbb3ee9c07346a67
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-26 15:03:29 -0300

    Create shopingcart

---
+++ dio_shoppingcart/src/main/java/com/dio/dio_shoppingcart/DioShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.dio.dio_shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioShoppingcartApplication.class, args);
+	}
+
+}
---
+++ dio_shoppingcart/src/test/java/com/dio/dio_shoppingcart/DioShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.dio_shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
