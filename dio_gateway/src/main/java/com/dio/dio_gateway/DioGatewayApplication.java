b0cc8b4a8595c20182c44cf0ccdff967fd233343
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-27 09:26:44 -0300

    Create Gateway

---
+++ dio_gateway/src/main/java/com/dio/dio_gateway/DioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dio.dio_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioGatewayApplication.class, args);
+	}
+
+}
